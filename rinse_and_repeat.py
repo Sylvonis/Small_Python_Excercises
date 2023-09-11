4a8672f2f9ad03c6e25d8458e0bd5faf3d86c3bc
Sylvio <Sylviococchiarellaneto94@gmail.com>
2023-09-11 15:03:38 -0300

    versão correta

--- rinse_and_repeat.py
+++ rinse_and_repeat.py
@@ -1,1 +1,20 @@
-PUTA MERDA APAGUEI O CODIGOAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+
+
+inputtext = input("digite algo para ser repetido")
+number = int(input("quantas vezes você deseja que essa frase seja repetida?"))
+
+
+def repetir (text, xtimes):
+    i = 0
+    while xtimes > i:
+        print(text)
+        i += 1
+
+
+repetir(inputtext, number)
+
+print("end")
+
+
+
+
