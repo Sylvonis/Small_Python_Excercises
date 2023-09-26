8d6981193271a0fc360935485c805f883404787c
Sylvio <Sylviococchiarellaneto94@gmail.com>
2023-09-26 15:51:18 -0300

    adding a second default text and tranforming textFinder program to a function

--- Python/textFinderFromFile.py
+++ Python/textFinderFromFile.py
@@ -2,39 +2,43 @@
 This is just a word finder that I made while studying regular expressions
 
 You input a word/value/number and the program finds the values in the default_text.txt, show to you the value
-and in which line it was found
+and in which line it was found. The program does differentiate lower case and upper case, so Et and et both
+have different results
 
 The default text is just the Lorem Ipsum default text that you can find anywhere in the internet
 """
 
 import re
 
-lineNumber = 1
-found_COUNTER = 0
-found = False
-
-
 DEFAULT_TEXT = open('default_text.txt', 'r')
-input_WORD = input('input a word to find: \n')
+DEFAULT_TEXT2 = open('default_text2.txt', 'r')
 
+input_WORD = input('input a word to find: \n')
 value = re.compile(r'\s' + input_WORD + r'\s')
 
 
-for line in DEFAULT_TEXT:
-    found_VALUE = re.findall(value, line)
+def wordFinder(word, text_var):
+    line_number = 1
+    found_counter = 0
+    found = False
 
-    for value in found_VALUE:
-        found_COUNTER += 1
+    for line in text_var:
+        found_value = re.search(word, line)
+        found_value_string = re.findall(word, line)
 
-    if found_VALUE:
-        print('found ' + str(found_COUNTER) + ' of the matching value: ' + str(found_VALUE) +
-              '\non line: ' + str(lineNumber))
-        print()
-        found = True
-    lineNumber += 1
-    found_COUNTER = 0
+        for __ in found_value_string:
+            found_counter += 1
+
+        if found_value:
+            print('found ' + str(found_counter) + ' of the matching value: ' + str(found_value.group()) +
+                  '\non line: ' + str(line_number))
+            print()
+            found = True
+        line_number += 1
+        found_counter = 0
+
+    if not found:
+        print('No value has been found')
 
 
-if not found:
-    print('No value has been found')
-
+wordFinder(value, DEFAULT_TEXT)
