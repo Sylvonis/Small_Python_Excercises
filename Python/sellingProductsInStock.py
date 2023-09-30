776e0424dbab9f3d3df595a3a981ab5827a6b516
Sylvio <Sylviococchiarellaneto94@gmail.com>
2023-09-30 04:52:15 -0300

    finished the program

--- Python/sellingProductsInStock.py
+++ Python/sellingProductsInStock.py
@@ -1,34 +1,80 @@
 """
-Altere o Proograma de forma a solicitar ao usuário o produto e a quantidade
+Altere o Programa de forma a solicitar ao usuário o produto e a quantidade
 vendida
+
+the things that I didin't code in this program are the ones with a '# ***' beside it
 """
 
+estoque = {"tomate": [1000, 2.30],  # ***
+           "alface": [500, 0.45],  # ***
+           "batata": [2000, 1.20],  # ***
+           "feijão": [100, 1.50]}  # ***
 
-estoque ={"tomate": [1000, 2.30],
-          "alface": [ 500, 0.45],
-          "batata": [2001, 1.20],
-          "feijão": [ 100, 1.50]}
-
-
-venda = [["tomate", 5], ["batata", 10], ["alface", 5]]
-total = 0
-
-print("Vendas:\n")
-
-for operacao in venda:
-    produto, quantidade = operacao
-    preco = estoque[produto][1]
-    custo = preco * quantidade
-    print(f"{produto:12}: {quantidade:3d} x {preco:6.2f} = {custo:6.2f}")
-    estoque[produto][0] -= quantidade
-    total += custo
+produtos_compra = []
 
 
-print(f"Custo total: {total:21.2f}\n")
+def produto():
+    produto_escolhido = None
+    quant_escolhida = None
+    stop = 0
 
-print("Estoque:\n")
+    while True:
+        produto_escolhido= input('Escolha o produto que quer comprar:\n').lower()
+        if produto_escolhido in estoque:
+            break
+        else:
+            print('Este Produto não existe em nosso estoque\n')
 
-for chave, dados in estoque.items():
-    print("Descrição: ", chave)
-    print("Quantidade: ", dados[0])
-    print(f"Preço: {dados[1]:6.2f}\n")
+    while True:
+        try:
+            quant_escolhida = int(input('escolha a quantidade que quer comprar. 0 para sair\n'))
+            if quant_escolhida == 0:
+                break
+            for value in estoque:
+                if [produto_escolhido] == [value]:
+                    if quant_escolhida <= int(estoque[value][0]):
+                        produtos_compra.append([produto_escolhido, quant_escolhida])
+                        return False
+                    elif quant_escolhida > int(estoque[value][0]):
+                        print('não temos essa quantidade de ' + f'{produto_escolhido}\n')
+        except:
+            pass
+
+
+def compra(escolha_produto):
+    print("Vendas:\n")  # ***
+    total = 0  # ***
+
+    for operacao in escolha_produto:  # ***
+        [produt, quantidade] = operacao  # ***
+        preco = estoque[produt][1]  # ***
+        custo = preco * quantidade  # ***
+        print(f"{produt:12}: {quantidade:3d} x {preco:6.2f} = {custo:6.2f}")  # ***
+        estoque[produt][0] -= quantidade  # ***
+        total += custo  # ***
+
+    print(f"Custo total : {total:21.2f}\n")  # ***
+
+
+def mostrarEstoque():
+    print("Estoque:\n")  # ***
+    max_len = len(max(estoque.keys()))
+
+    for chave, dados in estoque.items():  # ***
+        print("Descrição:".ljust(11), chave.rjust(max_len))
+        print("Quantidade:".ljust(11), str(dados[0]).rjust(max_len))
+        print("Preço:".ljust(11), f"{dados[1]:6.2f}\n".rjust(max_len))
+
+
+while True:
+    input_Char = input("Insira E para verificar o estoque, C para comprar e S para sair\n").upper()
+    if input_Char == 'E':
+        mostrarEstoque()
+
+    if input_Char == 'C':
+        produto()
+        compra(produtos_compra)
+
+    if input_Char == 'S':
+        break
+
